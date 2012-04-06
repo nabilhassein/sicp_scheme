758a06b8a91c52057a1fe173af4883195c83338d
Nabil Hassein <nabil.hassein@gmail.com>
2012-04-06 14:07:00 -0400

    improved indentation

--- ch1/11.scm
+++ ch1/11.scm
@@ -1,19 +1,17 @@
 (define (f n)
-    (if (< n 3)
-        n
-        (+ (f (- n 1))
-           (* 2 (f (- n 2)))
-           (* 3 (f (- n 3))))))
+  (if (< n 3)
+      n
+      (+ (f (- n 1))
+         (* 2 (f (- n 2)))
+         (* 3 (f (- n 3))))))
 
 (define (g n)
-    (define (g-iter a b c count)
-      ;; invariant: a = (f count), b = (f (+ count 1)), c = (f (+ count 2))
-      (if (= count n)
-          a
-          (g-iter b
-                  c
-                  (+ c
-                     (* 2 b)
-                     (* 3 a))
-                  (+ count 1))))
-    (g-iter 0 1 2 0))
+  (define (g-iter a b c count)
+    ;; invariant: a = (f count), b = (f (+ count 1)), c = (f (+ count 2))
+    (if (= count n)
+        a
+        (g-iter b
+                c
+                (+ c (* 2 b) (* 3 a))
+                (+ count 1))))
+  (g-iter 0 1 2 0))
