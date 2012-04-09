27452c2968de43a25e55a3349a95d61844d3f7dc
Nabil Hassein <nabil.hassein@gmail.com>
2012-04-09 15:56:33 -0400

    changed iterative-improve, sqrt to work with additional arguments

--- ch1/46.scm
+++ ch1/46.scm
@@ -1,17 +1,22 @@
 (define (iterative-improve good-enough? improve)
-  (lambda (guess)
-    (define (iter guess)
-      (if (good-enough? guess)
+  (lambda (guess x)
+    (define (iter guess x)
+      (if (good-enough? guess x)
           guess
-          (iter (improve guess))))
-  (iter guess)))
+          (iter (improve guess x) x)))
+  (iter guess x)))
+
+(define tolerance 0.00001)
 
 (define (sqrt x)
   (define (average x y)
     (/ (+ x y) 2))
-  (define (improve guess)
+  (define (improve guess x)
     (average guess (/ x guess)))
   (define (square x) (* x x))
-  (define (close? guess)
+  (define (close? guess x)
     (< (abs (- (square guess) x)) 0.001))
-  ((iterative-improve close? improve) 1.0))
+  ((iterative-improve close? improve) 1.0 x))
+
+;; (define (fixed-point f first-guess)
+;;   (
