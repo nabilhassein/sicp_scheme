5a0ae1fb322dd490744d26e9d26699fa23088817
Nabil Hassein <nabil.hassein@gmail.com>
2012-04-09 16:26:10 -0400

    completed chapter 1

--- ch1/46.scm
+++ ch1/46.scm
@@ -1,22 +1,28 @@
 (define (iterative-improve good-enough? improve)
-  (lambda (guess x)
-    (define (iter guess x)
-      (if (good-enough? guess x)
+  (lambda (guess)
+    (define (iter guess)
+      (if (good-enough? guess)
           guess
-          (iter (improve guess x) x)))
-  (iter guess x)))
+          (iter (improve guess))))
+    (iter guess)))
 
 (define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1)
+      (< (abs (- v1 (f v1))) tolerance))
+  (define (try guess)
+    (if (close-enough? guess)
+        guess
+        (try (f guess))))
+  ((iterative-improve close-enough? try) first-guess))
 
 (define (sqrt x)
   (define (average x y)
     (/ (+ x y) 2))
-  (define (improve guess x)
+  (define (improve guess)
     (average guess (/ x guess)))
   (define (square x) (* x x))
-  (define (close? guess x)
+  (define (close? guess)
     (< (abs (- (square guess) x)) 0.001))
-  ((iterative-improve close? improve) 1.0 x))
-
-;; (define (fixed-point f first-guess)
-;;   (
+  ((iterative-improve close? improve) 1.0))
